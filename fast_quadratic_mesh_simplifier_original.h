6215136a77a681db049939ae8f901f2f8f9837d0
Relintai <relintai@gmail.com>
2020-03-28 15:45:43 +0100

    Added one of the original c++ version. Added _original suffix to the c# port for now.

--- fast_quadratic_mesh_simplifier_original.h
+++ fast_quadratic_mesh_simplifier_original.h
@@ -1,5 +1,5 @@
-#ifndef FAST_QUADRATIC_MESH_SIMPLIFIER_H
-#define FAST_QUADRATIC_MESH_SIMPLIFIER_H
+#ifndef FAST_QUADRATIC_MESH_SIMPLIFIER_ORIGINAL_H
+#define FAST_QUADRATIC_MESH_SIMPLIFIER_ORIGINAL_H
 
 /*
 
@@ -28,9 +28,15 @@ SOFTWARE.
 
 #include "core/reference.h"
 
-#include "core/array.h"
+#include <limits>
 
-#include "simplify.h"
+#include "mesh_utils.h"
+
+#include "core/array.h"
+#include "core/pool_vector.h"
+#include "core/resource.h"
+
+using namespace FQMS;
 
 class FastQuadraticMeshSimplifier : public Reference {
 	GDCLASS(FastQuadraticMeshSimplifier, Reference);
@@ -59,6 +65,25 @@ public:
 	void simplify_mesh(float quality);
 	void simplify_mesh_lossless();
 
+	void update_mesh(int iteration);
+	void update_references();
+	void remove_vertex_pass(int startTrisCount, int targetTrisCount, double threshold, PoolVector<bool> *deleted0, PoolVector<bool> *deleted1, int *deletedTris);
+	void compact_mesh();
+	bool are_uvs_the_same(int channel, int indexA, int indexB);
+	double vertex_error(const SymmetricMatrix &q, const double x, const double y, const double z) const;
+	double calculate_error(const MUVertex &vert0, const MUVertex &vert1, Vector3 *result);
+	void update_triangles(int i0, int ia0, const MUVertex &v, PoolVector<bool> *deleted, int *deletedTriangles);
+	bool flipped(const Vector3 &p, int i0, int i1, const MUVertex &v0, PoolVector<bool> *deleted);
+	static Vector3 calculate_barycentric_coords(Vector3 const &point, Vector3 const &a, Vector3 const &b, Vector3 const &c);
+	void interpolate_vertex_attributes(int dst, int i0, int i1, int i2, const Vector3 &barycentricCoord);
+
+	void remove_doubles();
+	void remove_doubles_hashed();
+
+	static double min3(double val1, double val2, double val3) {
+		return (val1 < val2 ? (val1 < val3 ? val1 : val3) : (val2 < val3 ? val2 : val3));
+	}
+
 	FastQuadraticMeshSimplifier();
 	~FastQuadraticMeshSimplifier();
 
@@ -66,7 +91,21 @@ protected:
 	static void _bind_methods();
 
 private:
-	Simplify::FQMS simplify;
+	PoolVector<Vertex> _vertices;
+	PoolVector<int> _indices;
+
+	PoolVector<MUTriangle> _mu_triangles;
+	PoolVector<MUVertex> _mu_vertices;
+	PoolVector<MURef> _mu_refs;
+
+	double _vertex_link_distance_sqr = std::numeric_limits<double>::epsilon();
+	int _max_iteration_count;
+	double _agressiveness;
+	bool _enable_smart_link;
+	bool _preserve_border_dges;
+	bool _preserve_uv_seam_edges;
+	bool _preserve_uv_foldover_edges;
+	int _format;
 };
 
 #endif
